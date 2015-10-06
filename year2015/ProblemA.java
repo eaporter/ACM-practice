1a6339458e5407dfd6cf9e99f8b79f3c02507b21
Elizabeth Porter <porter.elizabeth.anne@gmail.com>
2015-10-06 19:38:50 -0400

    adding comments

--- year2015/ProblemA.java
+++ year2015/ProblemA.java
@@ -4,8 +4,21 @@ import java.util.Scanner;
 
 public class ProblemA {
 
+	/**
+	 * Program to calculate the largest decline in stock prices over an interval of time,
+	 * where the input (read from System.in) consists of 6 values:
+	 * 		p, a, b, c, d: parameters for calculating the price at time k according to
+	 * 		the formula price(k) = p * (sin(a * k + b) + cos(c * k + d) + 2).
+	 * 		n: the number of times 1,2,...n at which to consider the price
+	 * The largest decline is calculated by calculating the price at each time and tracking both
+	 * the maximum price seen so far and the maximum decline seen so far, so that if the decline
+	 * between the maximum and the current price is greater than the maximum decline thus far,
+	 * the decline is updated, resulting in the final decline representing the maximum decline
+	 * over the entire time period.
+	 */
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		// Read the six inputs p, a, b, c, d, and n
 		int p = s.nextInt();
 		int a = s.nextInt();
 		int b = s.nextInt();
@@ -13,20 +26,35 @@ public class ProblemA {
 		int d = s.nextInt();
 		int n = s.nextInt();
 		s.close();
+		// Initial maximum and decline are 0.
 		double max = 0.0;
-		double diff = 0.0;
+		double decline = 0.0;
+		// Loop through each time from 1 to n
 		for (int i = 1; i <= n; i++) {
 			double value = price(p, a, b, c, d, i) ;
-			if (value > max) {
+			if (value > max) { // Update the maximum price
 				max = value;
 			}
-			if (max - value > diff) {
-				diff = max - value;
+			if (max - value > decline) { // Update the maximum decline
+				decline = max - value;
 			}
 		}
-		System.out.println(diff);
+		// Output the maximum decline in price
+		System.out.println(decline);
 	}
 	
+	/**
+	 * Calculate the price at time k according to the formula
+	 * price(k) = p * (sin(a * k + b) + cos(c * k + d) + 2).
+	 * 
+	 * @param p constant
+	 * @param a constant
+	 * @param b constant
+	 * @param c constant
+	 * @param d constant
+	 * @param k time at which to calculate the price
+	 * @return price
+	 */
 	public static double price(int p, int a, int b, int c, int d, int k) {
 		return p * (Math.sin(a * k + b) + Math.cos(c * k + d) + 2);
 	}
--- year2015/ProblemB.java
+++ year2015/ProblemB.java
@@ -1,42 +1,105 @@
 package year2015;
 
+import java.util.Scanner;
+
+/**
+ * @author eaporter
+ */
 public class ProblemB {
 
 	/**
-	 * @param args
+	 * Program to calculate the time at which two polygons moving in the xy plane have maximum
+	 * overlap.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		Polygon firstPolygon = readInput(input);
+		Polygon secondPolygon = readInput(input);
+		
+		// TODO minimize distance between polygon centers
+		
+		// TODO calculate overlap at minimized distance
+		
+		// TODO write output
 
 	}
 	
-
+	public static Polygon readInput(Scanner input) {
+		int n = input.nextInt();
+		int[] x = new int[n];
+		int[] y = new int[n];
+		for (int i = 0; i < n; i++) {
+			x[i] = input.nextInt();
+			y[i] = input.nextInt();
+		}
+		int v_x = input.nextInt();
+		int v_y = input.nextInt();
+		return new Polygon(x, y, v_x, v_y);
+	}
 
 }
 
+/*
+ * Class representing a polygon with an arbitrary number of sides
+ */
 class Polygon {
-	Line[] sides;
-	int v;
+	Line[] sides;	// Sides of the polygon
+	int v_x;		// Velocity in the x direction
+	int v_y;		// Velocity in the y direction
 	
-	public Polygon(int[] x, int[] y, int v) {
+	/**
+	 * x and y must be the same length. The first point in x and y is assumed to also be the
+	 * last point.
+	 * @param x		x coordinates of the points in the polygon
+	 * @param y		y coordinates of the points in the polygon
+	 * @param v_x	Velocity of the polygon in the x direction
+	 * @param v_y	Velocity of the polygon in the y direction
+	 */
+	public Polygon(int[] x, int[] y, int v_x, int v_y) {
 		sides = new Line[x.length];
+		// Create lines from each point.
 		for (int i = 0; i < sides.length; i++) {
 			int j = i + 1 % sides.length;
 			sides[i] = new Line(new Point(x[i], y[i]), new Point(x[j], y[j]));
 		}
-		this.v = v;
+		this.v_x = v_x;
+		this.v_y = v_y;
 	}
 	
-	public void computeIntersection(Polygon other) {
+	/**
+	 * Method to compute the area of overlap between two polygons
+	 * @param other	polygon with which to compute the overlap
+	 * @return		overlapping area
+	 */
+	public double computeOverlap(Polygon other) {
 		
+		// TODO
+		
+		return 0.0;
+	}
+	
+	/**
+	 * Find the center of the polygon
+	 * @return
+	 */
+	public Point findCenter() {
+		
+		// TODO
+		
+		return null;
 	}
 }
 
+/*
+ * Class to represent a line, with start and end points
+ */
 class Line {
-	Point start;
-	Point end;
-	double m;
-	double b;
+	Point start; // Initial point on the line
+	Point end; // Final point on the line
+	double m; // Slope of the line
+	double b; // y-intercept of the line
 	
 	public Line(Point start, Point end) {
 		this.start = start;
@@ -45,13 +108,23 @@ class Line {
 		b = start.y - m * start.x;
 	}
 	
+	/**
+	 * Method to compute the point at which this line intersects with another line
+	 * @param other line with which this line may or may not intersect
+	 * @return		point at which the two lines intersect, if there is one
+	 * 				null otherwise
+	 */
 	public Point computeIntersection(Line other) {
 		
+		// TODO
 		
 		return null;
 	}
 }
 
+/*
+ * Class to represent a point in an xy coordinate plane
+ */
 class Point {
 	double x;
 	double y;
